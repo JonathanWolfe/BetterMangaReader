07c2ce4121fb12f499411a4ca2ba7717d90128ed
Jon Wolfe <jon@socialtopias.com>
2015-12-10 16:18:17 -0500

    Init new structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,60 @@
-var browserify = require('browserify'),
-	gulp = require('gulp'),
-	source = require('vinyl-source-stream'),
-	buffer = require('vinyl-buffer'),
-	uglify = require('gulp-uglify');
-
-gulp.task('browserify', function () {
-	gulp.task('browserify', function () {
-		var bundleStream = browserify('./ext/js/app.js')
-			.bundle()
-			.pipe(source('build.js'))
-			.pipe(buffer())
-			.pipe(uglify())
-			.pipe(gulp.dest('./ext/js/'));
+var gulp = require( 'gulp' );
+var plumber = require( 'gulp-plumber' );
+var babel = require( 'gulp-babel' );
+var uglify = require( 'gulp-uglify' );
+var cssnext = require( 'gulp-cssnext' );
 
 
-		var buildInject = browserify('./ext/src/inject/inject.js')
-			.bundle()
-			.pipe(source('inject-build.js'))
-			.pipe(buffer())
-			.pipe(uglify())
-			.pipe(gulp.dest('./ext/src/inject/'));
-	});
-});
+gulp.task( 'manifest', function () {
+	gulp.src( 'src/manifest.json' ).pipe( gulp.dest( 'ext' ) );
+} );
 
-gulp.task('watch', function () {
-	gulp.watch('./ext/js/app.js', ['browserify']);
-	gulp.watch('./ext/js/mirrors/**.js', ['browserify']);
-	gulp.watch('./ext/src/inject/inject.js', ['browserify']);
-});
 
-gulp.task('default', ['browserify', 'watch']);
-gulp.task('bundle', ['browserify']);
+gulp.task( 'icons', function () {
+	gulp.src( 'src/icons/**/*' ).pipe( gulp.dest( 'ext/icons' ) );
+} );
+
+
+gulp.task( 'html', function () {
+	gulp.src( 'src/html/**/*.html' ).pipe( gulp.dest( 'ext/html' ) );
+} );
+
+
+gulp.task( 'css', function () {
+	gulp.src( 'src/styles/*.css' )
+		.pipe( cssnext( {
+			compress: true
+		} ) )
+		.pipe( gulp.dest( 'ext/styles' ) )
+} );
+
+
+gulp.task( 'scripts', function () {
+	return gulp.src( 'src/scripts/**/*.js' )
+		.pipe( plumber( {
+			errorHandler: function ( error ) {
+				console.log( error.message );
+				this.emit( 'end' );
+			}
+		} ) )
+		.pipe( babel( {
+			presets: [ 'es2015' ]
+		} ) )
+		.pipe( uglify() )
+		.pipe( gulp.dest( 'ext/scripts/' ) )
+} );
+
+
+gulp.task( 'build', [ 'manifest', 'icons', 'html', 'scripts', 'css' ] );
+
+
+gulp.task( 'watch', [ 'build' ], function () {
+	gulp.watch( 'src/scripts/**/*.js', [ 'scripts' ] );
+	gulp.watch( 'src/styles/**/*.css', [ 'css' ] );
+	gulp.watch( 'src/icons/**/*', [ 'icons' ] );
+	gulp.watch( 'src/html/**/*.html', [ 'html' ] );
+	gulp.watch( 'src/manifest.json', [ 'manifest' ] );
+} );
+
+
+gulp.task( 'default', [ 'build' ] );
