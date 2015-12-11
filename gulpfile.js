bab00f6c35499f2e36e765e9ff0aced7d316de1f
Jon Wolfe <jon@socialtopias.com>
2015-12-11 16:33:59 -0500

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,68 @@
-var gulp = require( 'gulp' );
-var plumber = require( 'gulp-plumber' );
-var babel = require( 'gulp-babel' );
-var uglify = require( 'gulp-uglify' );
-var cssnext = require( 'gulp-cssnext' );
+const gulp = require( 'gulp' );
+const plumber = require( 'gulp-plumber' );
+const babel = require( 'gulp-babel' );
+const uglify = require( 'gulp-uglify' );
+const cssnext = require( 'gulp-cssnext' );
 
 
-gulp.task( 'manifest', function () {
+const plumberOptions = {
+	errorHandler: function handleError( error ) {
+		console.log( error.message );
+		this.emit( 'end' );
+	},
+};
+
+
+gulp.task( 'manifest', function copyManifest() {
 	gulp.src( 'src/manifest.json' ).pipe( gulp.dest( 'ext' ) );
 } );
 
 
-gulp.task( 'icons', function () {
+gulp.task( 'icons', function copyIcons() {
 	gulp.src( 'src/icons/**/*' ).pipe( gulp.dest( 'ext/icons' ) );
 } );
 
 
-gulp.task( 'html', function () {
+gulp.task( 'html', function copyHTML() {
 	gulp.src( 'src/html/**/*.html' ).pipe( gulp.dest( 'ext/html' ) );
 } );
 
 
-gulp.task( 'css', function () {
+gulp.task( 'css', function processCSS() {
 	gulp.src( 'src/styles/*.css' )
 		.pipe( cssnext( {
-			compress: true
+			compress: true,
 		} ) )
-		.pipe( gulp.dest( 'ext/styles' ) )
+		.pipe( gulp.dest( 'ext/styles' ) );
 } );
 
 
-gulp.task( 'scripts', function () {
+gulp.task( 'scripts', function processJS() {
 	return gulp.src( 'src/scripts/**/*.js' )
-		.pipe( plumber( {
-			errorHandler: function ( error ) {
-				console.log( error.message );
-				this.emit( 'end' );
-			}
-		} ) )
+		.pipe( plumber( plumberOptions ) )
 		.pipe( babel( {
-			presets: [ 'es2015' ]
+			presets: [ 'es2015' ],
 		} ) )
 		.pipe( uglify() )
-		.pipe( gulp.dest( 'ext/scripts/' ) )
+		.pipe( gulp.dest( 'ext/scripts/' ) );
+} );
+
+gulp.task( 'parsers', function processParsers() {
+	return gulp.src( 'src/parsers/**/*.js' )
+		.pipe( plumber( plumberOptions ) )
+		.pipe( babel( {
+			presets: [ 'es2015' ],
+		} ) )
+		.pipe( uglify() )
+		.pipe( gulp.dest( 'ext/parsers/' ) );
 } );
 
 
-gulp.task( 'build', [ 'manifest', 'icons', 'html', 'scripts', 'css' ] );
+gulp.task( 'build', [ 'manifest', 'icons', 'html', 'parsers', 'scripts', 'css' ] );
 
 
-gulp.task( 'watch', [ 'build' ], function () {
+gulp.task( 'watch', [ 'build' ], function watchFiles() {
+	gulp.watch( 'src/parsers/**/*.js', [ 'parsers' ] );
 	gulp.watch( 'src/scripts/**/*.js', [ 'scripts' ] );
 	gulp.watch( 'src/styles/**/*.css', [ 'css' ] );
 	gulp.watch( 'src/icons/**/*', [ 'icons' ] );
