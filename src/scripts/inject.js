300cbec74b89dda553fb5dc4bfb68ab0c0db7a98
Jon Wolfe <jon@socialtopias.com>
2015-12-15 14:41:19 -0500

    add some real injection code

--- src/scripts/inject.js
+++ src/scripts/inject.js
@@ -1,168 +1,74 @@
-function updateTrackingButtons() {
-	$( '#BMRControls' ).append( '<button id="check-track" disabled>Checking...</button>' );
-	$( '#BMRControls #go-track' ).remove();
-	$( '#BMRControls #stop-track' ).remove();
-}
+$( document ).ready( function initBMRInject() {
+	let clonedHTML = $( 'body' ).clone();
+	clonedHTML.bmrLiteSanitize();
+	clonedHTML = clonedHTML.html();
 
-function checkTracked( info ) {
-	chrome.runtime.sendMessage( {
-		isMangaTracked: info.name,
-	}, function ( isTrackedResponse ) {
-		console.log( 'check if tracking', isTrackedResponse );
+	let chapterInfo;
 
-		$( '#BMRControls #check-track' ).remove();
-
-		if ( isTrackedResponse[ 0 ] === true ) {
-			$( '#BMRControls' ).append( '<button id="stop-track">Stop Tracking</button>' );
-
+	const events = {
+		getImagesForChapter: () => {
 			chrome.runtime.sendMessage( {
-				updateMangaReadChapter: {
-					id: isTrackedResponse[ 1 ],
-					info: info,
-				},
-			}, function ( updateReadChapterResponse ) {
-				console.log( updateReadChapterResponse );
+				action: 'getImagesForChapter',
+				html: clonedHTML,
 			} );
-		} else {
-			$( '#BMRControls' ).append( '<button id="go-track">Track Manga</button>' );
+		},
+
+		putImagesForChapter: ( images ) => {
+			const scanContainer = $( chapterInfo.scanContainer );
+			scanContainer.empty();
+
+			for ( let i = 0; i < chapterInfo.pages.length; i += 1 ) {
+				scanContainer.append( '<img src="' + images[ i ] + '" />' );
+			}
+		},
+
+		parseChapterPage: () => {
+			chrome.runtime.sendMessage( {
+				action: 'parseChapterPage',
+				html: clonedHTML,
+			}, ( response ) => {
+				if ( response && response.type === 'success' ) {
+					chapterInfo = response.value;
+					events.getImagesForChapter();
+				}
+			} );
+		},
+
+		getChapterListFromProfile: () => {
+			chrome.runtime.sendMessage( {
+				action: 'getChapterListFromProfile',
+				html: clonedHTML,
+			} );
+		},
+	};
+
+	chrome.runtime.onMessage.addListener( function processMessage( message, sender ) {
+		console.group( 'message recieved - ' + message.action );
+		console.log( 'message', message );
+		console.log( 'sender', sender );
+		console.groupEnd();
+
+		if ( typeof events[ message.action ] === 'function' ) {
+			events[ message.action ].call( this, message.value );
 		}
 	} );
-}
 
-function buildControls( chapterInfo, chapters ) {
+	chrome.runtime.sendMessage( {
+		action: 'parseHTML',
+		html: clonedHTML,
+	}, ( response ) => {
+		if ( !response ) console.error( 'Chrome Extension Error: ', chrome.runtime.lastError );
 
-	const chapterSelectOptions = chapters.map( ( chapter ) => {
-		const isCurrent = parseFloat( chapter[ 0 ] ) === parseFloat( chapterInfo.currentChapter );
-		const selected = isCurrent ? 'selected' : '';
-
-		return `<option value="${chapter[ 2 ]}" ${selected}>Chapter ${chapter[ 0 ]}</option>`;
-	} ).join( '' );
-
-	const iconURL = chrome.runtime.getURL( '../icons/icon19.png' );
-
-	const bmrControls = `
-	<div id="BMRControls">
-		<img src="${iconURL}" alt="BetterMangaRead" />
-
-		<button id="go-prev">&laquo;</button>
-
-		<form>
-			<select>
-				${chapterSelectOptions}
-			</select>
-		</form>
-
-		<button id="go-next">&raquo;</button>
-	</div>`;
-
-	return bmrControls;
-}
-
-function bmrInject() {
-	$( 'body' ).append( `
-		<div class="loading-wrap">
-			<div class="loader"></div>
-			<h2>BetterMangaReader Loading...</h2>
-		</div>
-	` );
-
-	$( document ).ready( function () {
-		let mirror;
-
-		if ( mirror && mirror.isCurrentPageAChapterPage( document ) ) {
-
-			const chapters = mirror.getChaptersFromPage( document );
-			const chapterInfo = mirror.getInformationFromCurrentPage( document );
-
-			console.log( 'chapterInfo', chapterInfo );
-
-			mirror.removeRedundant( document );
-			mirror.doSomethingBeforeWritingScans( document );
-
-			const pages = mirror.getPages( document );
-			const whereWrite = mirror.whereDoIWriteScans( document );
-
-			$( whereWrite ).prepend( buildControls( chapterInfo, chapters ) );
-
-			updateTrackingButtons();
-			checkTracked( chapterInfo );
-
-			const chapterList = $( '#BMRControls option' );
-			const selectedChapter = chapterList.filter( ':selected' );
-
-			if ( selectedChapter.index() === 0 ) {
-				$( '#go-prev' ).prop( 'disabled', true );
+		console.log( 'parseHTML', response );
+		if ( response.type === 'success' ) {
+			if ( response.value.isChapterPage ) {
+				events.parseChapterPage();
 			}
-			if ( selectedChapter.index() === chapterList.length - 1 ) {
-				$( '#go-next' ).prop( 'disabled', true );
+			if ( response.value.isProfilePage ) {
+				events.getChapterListFromProfile();
 			}
-
-			$( '#BMRControls #go-prev:not(:disabled)' ).on( 'click', function () {
-				const currentIndex = selectedChapter.index();
-				const newLocation = chapterList.eq( currentIndex - 1 ).val();
-				document.location = newLocation;
-			} );
-			$( '#BMRControls #go-next:not(:disabled)' ).on( 'click', function () {
-				const currentIndex = selectedChapter.index();
-				const newLocation = chapterList.eq( currentIndex + 1 ).val();
-				document.location = newLocation;
-			} );
-			$( '#BMRControls select' ).on( 'change', function () {
-				document.location = $( 'option', this ).filter( ':selected' ).val();
-			} );
-
-			pages.forEach( function ( page, index ) {
-				const image = mirror.getImageFromPage( page );
-				$( whereWrite ).append( '<img src="' + image + '" alt="" id="image-' + index + '" class="BMR-img" />' );
-			} );
-
-			mirror.doAfterMangaLoaded( document );
-			$( '.loading-wrap' ).hide();
-
-			$( '#BMRControls' )
-				.on( 'click', '#go-track', function () {
-					console.log( 'attempted to track' );
-
-					updateTrackingButtons();
-
-					const fullInfo = {
-						"name": chapterInfo.name,
-						"mirror": mirror.mirrorName,
-						"url": chapterInfo.currentMangaURL,
-						"urlOfLatestRead": chapterInfo.currentChapterURL,
-						"isTracked": true,
-						"latestRead": chapterInfo.currentChapter,
-						"latest": chapters[ chapters.length - 1 ][ 0 ],
-						"tags": [],
-						"chapterList": chapters,
-					};
-
-					chrome.runtime.sendMessage( {
-						mangaToTrack: fullInfo,
-					}, function ( response ) {
-						console.log( response );
-
-						checkTracked( chapterInfo );
-					} );
-				} )
-				.on( 'click', '#stop-track', function () {
-					console.log( 'attempting to stop tracking' );
-
-					updateTrackingButtons();
-
-					chrome.runtime.sendMessage( {
-						mangaToStopTracking: chapterInfo.name,
-					}, function ( response ) {
-						console.log( response );
-
-						checkTracked( chapterInfo );
-					} );
-				} );
 		} else {
-			$( '.loading-wrap' ).hide();
+			console.error( 'BMR Error: ', response.value );
 		}
-
 	} );
-}
-
-bmrInject();
+} );
