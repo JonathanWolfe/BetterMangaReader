83447852224d5f62235b182a9035692526e5e351
Jon Wolfe <jon@socialtopias.com>
2015-12-15 14:40:44 -0500

    mostly done parser interface

--- src/scripts/parsers.js
+++ src/scripts/parsers.js
@@ -1,20 +1,41 @@
-/**
- * The collection of available, registered parsers
- * @type {Object}
- */
-const collection = {};
+window.parsers = ( function initParsers() {
+	return {
+		/**
+		 * Add a parser to the Collection
+		 * @param {Parser} parser The parser object to be added
+		 */
+		register: ( parser ) => {
+			if ( !window.parsers[ parser.normalizedName ] ) {
+				window.parsers[ parser.normalizedName ] = parser;
+			}
+		},
 
-/**
- * Add a parser to the Collection
- * @param {Parser} parser The parser object to be added
- */
-function register( parser ) {
-	if ( !collection[ parser.normalizedName ] ) {
-		collection[ parser.normalizedName ] = parser;
-	}
-}
+		/**
+		 * Find a parser via a URL
+		 * @param  {String} url URL to search with
+		 * @return {Parser}     A Parser Object
+		 */
+		findByUrl: ( url ) => {
+			const pieces = new window.URL( url ).hostname.split( '.' );
+			let parser;
+			while ( pieces ) {
+				parser = window.parsers[ pieces.shift() ];
+				if ( parser ) break;
+			}
+			return parser || false;
+		},
 
-window.parsers = {
-	collection,
-	register,
-};
+		helpers: {
+			/**
+			 * Retrieves the HTML of a URL
+			 * @param  {String} url URL to scrape
+			 * @return {Promise}     Resolves with the HTML
+			 */
+			getHtmlFromUrl: ( url ) => fetch( url ).then(
+				( response ) => response.text().then(
+					( HTML ) => $( '<html />' ).append( HTML ).bmrLiteSanitize()
+				)
+			),
+		},
+	};
+}() );
