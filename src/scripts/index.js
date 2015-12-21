c3f3e49b21399b1643bfb264def83531e9f9240d
Jon Wolfe <jon@socialtopias.com>
2015-12-21 16:59:20 -0500

    add sorting and click handler

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,54 @@
-function bmrInit() {
-
-	window.data.getAll().then( ( bmrData ) => {
-		console.log( 'bmrData', bmrData );
-
-		const mangaTable = $( '#manga-table tbody' );
-
-		Object.keys( bmrData.tracking ).forEach( ( uuid ) => {
-			const manga = bmrData.tracking[ uuid ];
-			mangaTable.append( `
-				<tr id="${uuid}">
-					<td data-title="Mark Read" class="manga-markRead">
-						<button class="pure-button pure-button-warning">Mark Read</button>
-					</td>
-					<td data-title="Manga Name" class="manga-name">${manga.name}</td>
-					<td data-title="Read To" class="manga-readTo">${manga.readTo}</td>
-					<td data-title="Latest" class="manga-latest">${manga.latestChapter}</td>
-				</tr>
-			` );
-		} );
-	} );
-
+function sortByMangaName( a, b ) {
+	return $( '.manga-name', a ).text().localeCompare( $( '.manga-name', b ).text() );
 }
 
-window.data.saveChanges().then( bmrInit );
+function createMangaTable( bmrData ) {
+	const mangaTable = $( '#manga-table tbody' );
+	const rows = [];
+
+	Object.keys( bmrData.tracking ).forEach( ( uuid ) => {
+		const manga = bmrData.tracking[ uuid ];
+		const row = $( `
+				<tr id="${uuid}">
+					<td class="manga-markRead">
+						<button class="pure-button pure-button-warning">Mark Read</button>
+					</td>
+					<td class="manga-name">${manga.name}</td>
+					<td class="manga-readTo">${manga.readTo}</td>
+					<td class="manga-latest">${manga.latestChapter}</td>
+				</tr>
+			` );
+
+		if ( parseFloat( manga.latestChapter ) > parseFloat( manga.readTo ) ) {
+			row.addClass( 'has-unread' );
+		}
+
+		rows.push( row );
+	} );
+
+	const unread = rows.filter( ( row ) => row.hasClass( 'has-unread' ) ).sort( sortByMangaName );
+	const read = rows.filter( ( row ) => !row.hasClass( 'has-unread' ) ).sort( sortByMangaName );
+
+	console.log( 'unread', unread );
+	console.log( 'read', read );
+
+	mangaTable.prepend( unread );
+	mangaTable.append( read );
+
+	return mangaTable;
+}
+
+function bmrInit( bmrData ) {
+	console.log( 'bmrData', bmrData );
+
+	createMangaTable( bmrData );
+
+	$( '#manga-table' ).on( 'click', 'tbody tr', function mangaClicked( event ) {
+		const uuid = event.currentTarget.id;
+		const manga = bmrData.tracking[ uuid ];
+
+		window.location.assign( manga.url );
+	} );
+}
+
+window.data.saveChanges().then( window.data.getAll ).then( bmrInit );
